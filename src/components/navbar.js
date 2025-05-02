78ef45d52c2f7bbf4aa6309e4ab8b826cfb0f8ae
Sabri Eyuboglu <eyuboglu@stanford.edu>
2025-05-02 14:48:52 -0700

    Update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -81,25 +81,26 @@ export default class NavBar extends Component {
                             I am supported by the <a href="https://www.nsfgrfp.org/"> National Science Foundation GRFP </a>.
                         </p>
                         <p>
-                            <b>Research Interests.</b> I'm currently working on improving LLM <i>memory</i>: the ability to store and recall large amounts of information. 
+                            <b>Research Interests.</b> I'm currently working on improving LLM <i>memory</i>, the ability of a model to store and recall large amounts of user-provided information. 
                             In particular, I'm interested in the tradeoff between cost (<i>e.g.</i> memory consumption, decoding latency and throughput) and quality. 
                              <i> How should we balance this tradeoff as we scale up the amount of information we want to remember?</i> 
                             
                         </p>
                         <p>
-                            Answering questions like this one is critical for realizing more personalized, sustainable, accessible, and effective AI.   
-                            To answer them, I spend a lot of time trying to understand the internals of how language models work. I do this by exploring the swaths of data on which they are trained and formulating simple synthetic tasks [<a href="https://arxiv.org/abs/2312.04927">1</a>,<a href="https://arxiv.org/abs/2402.18668">2</a>,<a href="https://openreview.net/pdf?id=2N3CtUdoB0">3</a>].
-                            Informed by this understanding, I spend (probably too much) time designing new architectures (<i>e.g.</i> Based) and training them from scratch [<a href="https://arxiv.org/abs/2402.18668">4</a>,<a href="https://arxiv.org/abs/2310.12109">5</a>]. 
-                            and building systems that combine multiple LLMs together with LLM-generated code to more efficiently process large amounts of information[<a href="https://arxiv.org/abs/2502.15964">6</a>,<a href="https://arxiv.org/abs/2304.09433">7</a>].
+                            Answering this question is critical for realizing more personalized, sustainable, accessible, and effective AI.   
+                            In an effort to answer it, I spend a lot of time trying to understand the internals of how language models work. 
+                            I do this by exploring the swaths of data on which they are trained and formulating simple synthetic tasks [<a href="https://arxiv.org/abs/2312.04927">1</a>,<a href="https://arxiv.org/abs/2402.18668">2</a>,<a href="https://openreview.net/pdf?id=2N3CtUdoB0">3</a>].
+                            Informed by this understanding, I spend (probably too much) time designing new architectures (<i>e.g.</i> Based) and training them from scratch [<a href="https://arxiv.org/abs/2402.18668">4</a>,<a href="https://arxiv.org/abs/2310.12109">5</a>].  
+                            I also have spent time developing LLM-systems that can efficiently process large amounts of information by combining multiple LLMs and LLM-generated code [<a href="https://arxiv.org/abs/2502.15964">6</a>,<a href="https://arxiv.org/abs/2304.09433">7</a>].
                         </p>
                         <p>
                             <b>Background.</b> In the early days of my PhD, I had a lot of fun developing techniques (<i>e.g.</i> Domino [<a href="https://arxiv.org/abs/2304.09433">8</a>], Mocha [<a href="https://facctconference.org/static/papers24/facct24-162.pdf">9</a>]) and tools (Meerkat [<a href="https://github.com/HazyResearch/meerkat">10</a>]) to help deep learning practitioners identify subtle failure modes of their models. 
-                            The big idea in this line of work (which perhaps seems obvious in hindsight) was to use "foundation models" (e.g. CLIP, GPT-3), which were brand new at the time, to help analyze and wrangle unstructured validation data.
+                            The big idea in this line of work was to use "foundation models" (e.g. CLIP, GPT-3), which were brand new at the time, to help analyze and wrangle unstructured validation data.
                         </p>
                         <p>
-                            Prior to my PhD, my research was motivated by challenges that arise speficially when trying to apply machine learning in safety-critical settings like medicine and the sciences [<a href="https://www.nature.com/articles/s41467-021-22018-1">11</a>, <a href="https://cs.stanford.edu/people/sabrieyuboglu/psb-mi.pdf">12</a>]. 
-                            I was a machine learning researcher at <a href="https://flatiron.com/">Flatiron Health</a>. 
-                            I completed my undergrad and master's at Stanford, where I worked with Jure Leskovec's
+                            Prior to my PhD, my research focused on applying machine learning in safety-critical settings like medicine and the sciences [<a href="https://www.nature.com/articles/s41467-021-22018-1">11</a>, <a href="https://cs.stanford.edu/people/sabrieyuboglu/psb-mi.pdf">12</a>]. 
+                            I was a machine learning researcher at <a href="https://flatiron.com/">Flatiron Health</a> for a year. 
+                            Before that, I completed my undergrad and master's at Stanford, where I worked with Jure Leskovec's
                             <a href="https://cs.stanford.edu/~jure/"> SNAP Group</a> and the <a href="https://aimi.stanford.edu/">AIMI Center</a>. 
                             I'm grateful to my many mentors along the way who helped me navigate my early days in research including (but not limited to).
                         </p>
